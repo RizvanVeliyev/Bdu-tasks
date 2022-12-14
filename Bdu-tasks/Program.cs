56be8df584bc5c1d43ab4043f47ad927dee41f7e
RizvanVeliyev <veliyevrizvan250@gmail.com>
2022-12-14 07:42:54 +0400

    Add project files.

---
+++ Bdu-tasks/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Bdu_tasks
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			Console.Write("a:");
+			int a = int.Parse(Console.ReadLine());
+			for (int i=1; i <= a; i++)
+			{
+				Console.WriteLine($"{i}-ci eded:{i}");
+			}
+		}
+	}
+}
